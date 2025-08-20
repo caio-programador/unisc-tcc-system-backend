e79132e2f524d295e36090bb211cef716319a7d1
caio-programador <caio.florees2005@gmail.com>
2025-08-20 15:17:13 -0300

    feat: initial commit

---
+++ src/main/java/br/com/unisc/unisctccsystembackend/UniscTccSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.unisc.unisctccsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniscTccSystemBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UniscTccSystemBackendApplication.class, args);
+  }
+
+}
