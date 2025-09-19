b6aa52309e635ff1793c8ad3374a2a1d2d05d2b3
Caio Flôres <112014273+caio-programador@users.noreply.github.com>
2025-09-18 21:25:41 -0300

    chore: removing unused annotation

--- src/main/java/br/com/unisc/unisctccsystembackend/UniscTccSystemBackendApplication.java
+++ src/main/java/br/com/unisc/unisctccsystembackend/UniscTccSystemBackendApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = "br.com.unisc.unisctccsystembackend")
-@EnableJpaRepositories(basePackages = "br.com.unisc.unisctccsystembackend")
 public class UniscTccSystemBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(UniscTccSystemBackendApplication.class, args);
